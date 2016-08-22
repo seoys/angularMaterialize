aff2f552d6284227cda485990931f96a57c319ed
seoys <sosohappy@29cm.co.kr>
2016-08-22 19:17:00 +0900

    AngularMaterlize 구성

--- app.js
+++ app.js
@@ -3,16 +3,26 @@ var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var bodyParser = require('body-parser')
+,swig = require('swig');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var api = require('./routes/api');
 
 var app = express();
 
 // view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
+//
+//
+
+var swig = new swig.Swig();
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('node_modules', path.join(__dirname, 'node_modules'));
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -24,6 +34,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/api',api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
