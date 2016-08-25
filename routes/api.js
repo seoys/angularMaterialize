5b34d9f31ee77e65bae80f855de97d80c0897b25
seoys <angelskying@gmail.com>
2016-08-25 16:32:58 +0900

    .

--- routes/api.js
+++ routes/api.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
- console.log("asdfasdfasdf");
+ console.log("asdfasdfasdf123123123");
  res.render('index');
 });
 
