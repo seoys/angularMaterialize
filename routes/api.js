6e08fd9eb49f62bb058a87312f495e60a9254ada
seoys <sosohappy@29cm.co.kr>
2016-08-25 10:30:11 +0900

    .

--- routes/api.js
+++ routes/api.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
- console.log("test123234234234");
+ console.log("asdfasdfasdf");
  res.render('index');
 });
 
